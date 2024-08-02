dbddd2c61284867121760e2e272c80c3a1cd8258
dschifel <danielschifelbain@gmail.com>
2024-08-02 10:54:21 -0300

    Created basis of project

---
+++ src/test/java/com/report_factory/report_factory/ReportFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.report_factory.report_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
